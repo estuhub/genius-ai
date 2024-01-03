910170e7295c80c8825503398ef54681e7d49bea
estuhub <107235863+estuhub@users.noreply.github.com>
2024-01-03 20:50:29 +0700

    create LandingNavbar component

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,16 +1,9 @@
-import { Button } from "@/components/ui/button"
-import Link from "next/link"
+import { LandingNavbar } from "@/components/landing-navbar"
 
 const LangingPage = () => {
     return (
-        <div>
-            <div>Landing Page (Unprotected)</div>
-            <Link href="/sign-in">
-                <Button>Login</Button>
-            </Link>
-            <Link href="/sign-up">
-                <Button>Register</Button>
-            </Link>
+        <div className="h-full">
+            <LandingNavbar />
         </div>
     )
 }
